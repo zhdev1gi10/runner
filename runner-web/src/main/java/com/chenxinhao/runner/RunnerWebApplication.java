24029500698575e1dc17ad8d1af52cf1203f572d
zhdev1gi10 <zaoguishu6213@126.com>
2021-04-14 23:26:09 +0800

    第三次提交

---
+++ runner-web/src/main/java/com/chenxinhao/runner/RunnerWebApplication.java
@@ -0,0 +1,11 @@
+package com.chenxinhao.runner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnerWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RunnerWebApplication.class, args);
+    }
+}
